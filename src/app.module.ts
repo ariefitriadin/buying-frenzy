6d18edd90502e9150cfa2b85da3c8b659536e73e
Arie Fitriadin <arie.fitriadin@larisin.id>
2020-08-12 16:22:31 +0700

    refactor: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
