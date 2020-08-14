38ac3e3679d6f8f6f2a7e5036a15b0cc7bd1a16d
Arie Fitriadin <arie.fitriadin@larisin.id>
2020-08-14 16:15:40 +0700

    chore: left behind initial commit base entity model
    it should included in initial model development
    but it left behind

---
+++ src/model/base.entity.ts
@@ -0,0 +1,18 @@
+import { PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn } from 'typeorm';
+
+export abstract class BaseEntity {
+    @PrimaryGeneratedColumn('increment', {type: 'int'})
+    id: number;
+
+    @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    createDateTime: Date;
+
+    @Column({ type: 'varchar', length: 150 })
+    createdBy: string;
+
+    @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    lastChangedDateTime: Date;
+
+    @Column({ type: 'varchar', length: 300 })
+    lastChangedBy: string;
+}
