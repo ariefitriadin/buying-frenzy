53b397fb6827c6946e100054de7b6cc302762e1a
Arie Fitriadin <arie.fitriadin@larisin.id>
2020-08-14 16:41:21 +0700

    feat: initial User modul with controller, service and entity

---
+++ src/model/user.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, OneToMany } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Order } from './order.entity';
+
+@Entity({ name: 'user'})
+export class User extends BaseEntity {
+
+    @Column({ type: 'varchar', length: 300})
+    name: string;
+
+    @Column({ type: 'float'})
+    cashBalance: number;
+
+    @OneToMany(type => Order, order => order.user)
+    orders: Order[]
+
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "../model/user.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserService {
+    constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
+
+    public async getAll() {
+        return await this.repo.find();
+    }
+}
